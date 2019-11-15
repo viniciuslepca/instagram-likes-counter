835ff695b7d86994ae802597a11b051cd95bcaa9
viniciuslepca <vinicius.lepca@gmail.com>
2019-11-15 00:41:26 -0500

    Finalized basic functionalities

--- instagram-like-counter-env/scraper.py
+++ instagram-like-counter-env/scraper.py
@@ -1,21 +1,15 @@
 import json
 import requests
 
-from bs4 import BeautifulSoup
-
 class Scraper(object):
-    def __init__(self):
-        #self.url = "https://www.instagram.com/p/B4SDBGCHL6y/"
-        print("Insert url of public picture")
-        self.url = input()
+    def __init__(self, url):
+        self.url = url
 
     def scrape_site(self):
         text = requests.get(self.url).text
         content_to_search = "<meta content=\""
         start_index = text.find(content_to_search)
         likes_word_index = text.find("Likes", start_index)
+        if likes_word_index == -1:
+            return -1;
         return text[(start_index + len(content_to_search)):likes_word_index]
-        
-scraper = Scraper()
-print()
-print("Number of likes: " + scraper.scrape_site())
